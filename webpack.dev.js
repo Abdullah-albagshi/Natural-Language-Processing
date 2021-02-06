2edf9841fd35d8b27416a85003a4d387a7ba466c
Abdullah albagshi <abdullah.albagshi@gmail.com>
2021-02-06 17:44:10 +0300

    :sparkles: update configs

---
+++ webpack.dev.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebBackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.min.js',
+        libraryTarget: 'var',
+        library: 'Client',
+    },
+    devtool: 'source-map',
+    stats: 'verbose',
+    module: {
+        rules: [{
+                test: '/.js$/',
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebBackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html',
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false,
+        }),
+        // new BundleAnalyzerPlugin()
+    ],
+};
