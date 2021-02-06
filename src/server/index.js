8b480078684df669115696ba5935e933e9a65cf0
Abdullah albagshi <abdullah.albagshi@gmail.com>
2021-02-06 17:36:52 +0300

    :sparkles: setup server

---
+++ src/server/index.js
@@ -0,0 +1,24 @@
+// Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Start up an instance of app
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
+app.use(express.static('dist'));
+
+app.get('/', function(req, res) {
+    res.sendFile('dist/index.html');
+});
+
+// set port to be 3000 or a given port in .env config
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
