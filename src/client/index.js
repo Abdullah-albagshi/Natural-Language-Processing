930e1b2aa551ec7995619d61263e1e17a022e4b6
Abdullah albagshi <abdullah.albagshi@gmail.com>
2021-02-07 16:46:26 +0300

    import js and scss files

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import { key } from './js/fetchApiKey';
+import { handleForm } from './js/formHandler';
+import { validURL } from './js/urlValidator';
+
+import './styles/styles.scss';
+
+export { key, handleForm, validURL };
